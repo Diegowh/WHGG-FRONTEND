f26edb6b5be18581a1ea6aa6edcaebf1ba7ff674
diegowh <contacto.diegowh@gmail.com>
2025-09-10 17:19:40 +0200

    Add API functions for league entries, champion stats, and matches

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,15 @@
-import type {ProfileData} from "../types";
+import type {AccountData, LeagueEntryData, ChampionStatsData, MatchData } from "../types";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
+const API_BASE_URL = 'http://localhost:8080/api';
 
 export async function searchAccount(
     server: string,
     gameName: string,
     tagLine: string
-): Promise<ProfileData> {
+): Promise<AccountData> {
 
     const response = await fetch(
-        `${API_BASE_URL}/api/profile/${server}/${encodeURIComponent(gameName)}/${encodeURIComponent(tagLine)}`
+        `${API_BASE_URL}/v1/account/${server.toLowerCase()}/${encodeURIComponent(gameName.toLowerCase())}-${encodeURIComponent(tagLine.toLowerCase())}`
     );
 
     if (!response.ok) {
@@ -18,3 +18,53 @@ export async function searchAccount(
 
     return response.json();
 }
+
+export const fetchLeagueEntries = async (
+    server: string,
+    gameName: string,
+    tagLine: string
+): Promise<LeagueEntryData[]> => {
+    const response = await fetch(
+        `${API_BASE_URL}/v1/ranked/${server.toLowerCase()}/${encodeURIComponent(gameName.toLowerCase())}-${encodeURIComponent(tagLine.toLowerCase())}`
+    )
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch league entries ${response.status}: ${response.statusText}`);
+    }
+
+    return response.json();
+};
+
+export const fetchChampionStats = async (
+    gameName: string,
+    tagLine: string
+): Promise<ChampionStatsData[]> => {
+
+    const response = await fetch(
+        `${API_BASE_URL}/v1/champion/stats/${encodeURIComponent(gameName.toLowerCase())}-${encodeURIComponent(tagLine.toLowerCase())}/ranked`
+    );
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch champion stats: ${response.statusText}`);
+    }
+
+    return response.json();
+};
+
+
+export const fetchMatches = async (
+    server: string,
+    gameName: string,
+    tagLine: string
+): Promise<MatchData[]> => {
+
+    const response = await fetch(
+        `${API_BASE_URL}/v1/matches/${server.toLowerCase()}/${encodeURIComponent(gameName.toLowerCase())}-${encodeURIComponent(tagLine.toLowerCase())}`
+    );
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch matches: ${response.statusText}`);
+    }
+
+    return response.json();
+};
