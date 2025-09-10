131d135e209bd96779c949913860c5299ab831eb
diegowh <contacto.diegowh@gmail.com>
2025-09-10 17:19:03 +0200

    Create servers.ts

---
+++ src/constants/servers.ts
@@ -0,0 +1,20 @@
+
+
+export const SERVERS = [
+    { value: 'euw1', label: 'EUW' },
+    { value: 'eun1', label: 'EUNE' },
+    { value: 'na1', label: 'NA' },
+    { value: 'br1', label: 'BR' },
+    { value: 'ru', label: 'RU' },
+    { value: 'kr', label: 'KR' },
+    { value: 'jp1', label: 'JP' },
+    { value: 'tw2', label: 'TW' },
+    { value: 'tr1', label: 'TR' },
+    { value: 'ph2', label: 'PH' },
+    { value: 'vn2', label: 'VN' },
+    { value: 'la1', label: 'LAN' },
+    { value: 'la2', label: 'LAS' },
+    { value: 'oc1', label: 'OC' },
+    { value: 'sg2', label: 'SG' },
+    { value: 'th2', label: 'TH' },
+] as const;
